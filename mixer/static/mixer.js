hachamge/mixer

commit 6a7d2acd7aa431a7d84c04ade55892dfc8f73c5d

Edit each value in the rgb p tag header to show color
There is a bug introduced that causes the values to not be updated when
a cache color is selected. The screenshot and the mixer background
reflect the change, exept for the rgb p tag header

mixer/static/mixer.css
@@ -66,7 +66,7 @@ body {
 
 .config-main > p {
 	padding: 5px;
-	color: #720e9e;
+	color: gray;
 	height: 50px;
 	font-size: 1.7em;
 	text-align: center;
@@ -159,4 +159,14 @@ body {
 
 .config-main :nth-child(5) input[type="range"]::-webkit-slider-runnable-track {
 	background: blue;
+}
+
+#red {
+	color: red;
+}
+#green {
+	color: green;
+}
+#blue {
+	color: blue;
 }

mixer/static/mixer.js
@@ -6,6 +6,9 @@ var color_cache = document.querySelector(".color-cache");
 var primary_color = document.querySelector("#primary-color");
 var screenshot = document.querySelector(".screenshot");
 var screen = document.querySelector(".mixer");
+var redMarker = document.querySelector("#red");
+var greenMarker = document.querySelector("#green");
+var blueMarker = document.querySelector("#blue");
 
 function cache() {
 	var color = document.createElement("div");
@@ -22,7 +25,9 @@ document.querySelector(".color-cache").addEventListener("click", function() {
 
 	for(var i = 0; i < size; ++i) {
 		colors[i].onclick = function() {
-			primary_color.textContent = this.style.backgroundColor;
+			redMarker.textContent = red;
+			greenMarker.textContent = green;
+			blueMarker.textContent = blue;
 			screen.style.backgroundColor = this.style.backgroundColor;
 			screenshot.style.backgroundColor = this.style.backgroundColor;
 		}
@@ -35,7 +40,7 @@ document.querySelector("#red-slider").addEventListener("change", function() {
 	green = document.querySelector("#green-slider").value;
 	blue = document.querySelector("#blue-slider").value;
 
-	primary_color.textContent = `rgb(${this.value}, ${green}, ${blue})`;
+	redMarker.textContent = red;
 	cache();
 	document.querySelector(".screenshot").style.backgroundColor = `rgb(${this.value},${green},${blue})`;
 })
@@ -46,7 +51,7 @@ document.querySelector("#green-slider").addEventListener("change", function() {
 	red = document.querySelector("#red-slider").value;
 	blue = document.querySelector("#blue-slider").value;
 
-	primary_color.textContent = `rgb(${red}, ${green}, ${blue})`;
+	greenMarker.textContent = green;
 	cache();
 	document.querySelector(".screenshot").style.backgroundColor = `rgb(${red},${green},${blue})`;
 })
@@ -57,7 +62,7 @@ document.querySelector("#blue-slider").addEventListener("change", function() {
 	red = document.querySelector("#red-slider").value;
 	green = document.querySelector("#green-slider").value;
 
-	primary_color.textContent = `rgb(${red}, ${green}, ${blue})`;
+	blueMarker.textContent = blue;
 	cache();
 	document.querySelector(".screenshot").style.backgroundColor = `rgb(${red},${green},${blue})`;
 })

mixer/templates/mixer/mixer.html
@@ -17,7 +17,7 @@
 				 </div>
 
 				 <div class="config-main">
-					 <p id="primary-color">rgb(52, 71, 106)</p>
+					 <p id="primary-color">rgb(<b id=red>52</b>, <b id=green>71</b>, <b id=blue>106</b>)</p>
 
 					 <div class="screenshot"></div>
 

commit f5ab12f8c08de1e4b984e70e17e1531159bbe75a

Resolved the bug introduced in commit: 6a7d2ac
The bug made it so that the rgb header values were not updated when a
cache color was selected. To resolve this the rgbParser function was
created to parsed the background color, and return an array with each
value inside

mixer/static/mixer.css
@@ -24,7 +24,7 @@ body {
 	align-items: center;
 	overflow: hidden;
 	transition: 2s;
-  background-color: #C9D7DD;
+  background-color: rgb(81, 104, 106);
 }
 
 .config {
@@ -77,7 +77,7 @@ body {
 .screenshot {
 	width: 87%;
 	height: 300px;
-	background-color: #C9D7DD;
+	background-color: rgb(81, 104, 106);
 	margin: 0 auto;
 	transition: 1s;
 }

mixer/static/mixer.js
@@ -17,6 +17,27 @@ function cache() {
 	color_cache.append(color);
 }
 
+function rgbParser(unparsedInput) {
+	let size = unparsedInput.length;
+	var rgbParsedValues = []
+	var numberString = '';
+
+	for(var i = 0; i < size; ++i) {
+		if(!isNaN(unparsedInput[i])) {
+			for(var j = i; !isNaN(unparsedInput[j]); ++j) {
+				numberString += unparsedInput[j];
+			}
+			rgbParsedValues.push(Number(numberString));
+			numberString = '';
+			i = j;
+			j = 0;
+		} else {
+			continue;
+		}
+	}
+
+	return rgbParsedValues;
+}
 
 // update screenshot
 document.querySelector(".color-cache").addEventListener("click", function() {
@@ -25,9 +46,12 @@ document.querySelector(".color-cache").addEventListener("click", function() {
 
 	for(var i = 0; i < size; ++i) {
 		colors[i].onclick = function() {
-			redMarker.textContent = red;
-			greenMarker.textContent = green;
-			blueMarker.textContent = blue;
+			let values = rgbParser(this.style.backgroundColor);
+
+			redMarker.textContent = values[0];
+			greenMarker.textContent = values[1];
+			blueMarker.textContent = values[2];
+
 			screen.style.backgroundColor = this.style.backgroundColor;
 			screenshot.style.backgroundColor = this.style.backgroundColor;
 		}

mixer/templates/mixer/mixer.html
@@ -17,7 +17,7 @@
 				 </div>
 
 				 <div class="config-main">
-					 <p id="primary-color">rgb(<b id=red>52</b>, <b id=green>71</b>, <b id=blue>106</b>)</p>
+					 <p id="primary-color">rgb(<b id=red>81</b>, <b id=green>104</b>, <b id=blue>106</b>)</p>
 
 					 <div class="screenshot"></div>
 